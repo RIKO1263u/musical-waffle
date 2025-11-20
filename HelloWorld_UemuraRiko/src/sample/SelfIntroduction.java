29ebc61b68cb174aa949736abf6cf8fad4b1532b
rriko <rriko@192.168.0.30>
2025-11-20 23:26:54 +0900

    Java導入3　課題

---
+++ HelloWorld_UemuraRiko/src/sample/SelfIntroduction.java
@@ -0,0 +1,24 @@
+package sample;
+
+public class SelfIntroduction {
+
+	public static void main(String[] args) {
+	   // 氏名
+		String name ="上村莉子";
+	    // 年齢
+		int age = 25;
+		// 趣味
+	    String hobby ="ゲーム";
+	    
+	    System.out.println("私の名前は"+ name +"です。");
+	    System.out.println("年齢は"+ age +"です。");
+	    System.out.println("趣味は"+ hobby +"です。"); 
+	    		
+	    
+
+	}
+	/*
+	 * 自己紹介文を出力する処理
+	 */
+
+}
